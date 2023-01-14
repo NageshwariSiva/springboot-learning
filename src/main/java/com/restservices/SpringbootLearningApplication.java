751c766d129f667540313642268d1b4e3b7b5114
Nageshwari S <jegadharuthvi@gmail.com>
2023-01-14 16:10:17 +0530

    second commit

--- src/main/java/com/restservices/SpringbootLearningApplication.java
+++ src/main/java/com/restservices/SpringbootLearningApplication.java
@@ -3,7 +3,7 @@ package com.restservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//second committ
+//
 @SpringBootApplication
 public class SpringbootLearningApplication {
 
